df04be02fba22456f2de0ecdb546b2d94219949d
Jim <jdeselms@vistaprint.com>
2022-02-10 18:30:05 -0500

    Initial

---
+++ src/index.js
@@ -0,0 +1,50 @@
+function buildResponse(...contexts) {
+    const merged = Object.assign({}, ...contexts)
+
+    return chainify(merged)
+}
+
+function chainify(context) {
+    if (context === null || typeof context !== "object") {
+        return context
+    }
+
+    return new Proxy(context, {
+        get: (target, prop, handler) => {
+            if (prop === "then") {
+                if (target.then) {
+                    return target.then
+                } else {
+                    return target
+                }
+            }
+
+            const next = target[prop]
+            if (typeof next === "function") {
+                const result = next()
+                if (result.then) {
+                    return result
+                } else {
+                    return chainify(result)
+                }
+            }
+            return chainify(next)
+        }
+    })
+}
+
+module.exports = {
+    buildResponse
+}
+
+
+
+
+
+/*
+
+What I want is to return a promise which also happens to have the properties of the next thing in the chain.
+So, if you call "then" on the thing, then you'll resolve the promise.
+// If you get one of the other properties, then you'll get the other thing.
+
+*/
---
+++ src/index.test.js
@@ -0,0 +1,56 @@
+const { buildResponse } = require("./index.js")
+
+describe("buildResponse", () => {
+    it("works", async () => {
+        const resp = buildResponse({a: 1, b: { c: 3 }})
+
+        expect(await resp.a).toBe(1)
+        expect(await resp.b.c).toBe(3)
+    })
+
+    it("can handle merged objects, and later objects replace earlier ones", async () => {
+        const resp = buildResponse({a: {b: { c: 3}}})
+
+        expect(await resp.a.b.c).toBe(3)
+    })
+
+    it("is harmless to await intermediate results", async () => {
+        const resp = await buildResponse({a: {b: { c: 3}}})
+ 
+        const a = await resp.a
+        const b = await a.b
+        const c = await b.c
+
+        expect(c).toBe(3)
+    })
+
+    it("works with arrays", async () => {
+        const resp = buildResponse([1, 2])
+
+        expect(await resp[0]).toBe(1)
+        expect(await resp[1]).toBe(2)
+    })
+
+    it("works with an object that has a then that isn't a promise", async () => {
+        const resp = buildResponse({ then: 123 })
+
+        expect(await resp.then).toBe(123)
+    })
+
+    it("works with functions", async () => {
+        const resp = buildResponse({ a: 5000 })
+
+        expect(await resp.a).toBe(5000)
+    })
+
+    it("works with promises", async () => {
+        const resp = buildResponse({ a: () => delayed(500) })
+
+        expect(await resp.a).toBe(500)
+    })
+})
+
+async function delayed(response) {
+    await new Promise(r => setTimeout(r, 1))
+    return response
+}
