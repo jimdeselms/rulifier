ab3617611c518ba56f675630d05a07648ce16a05
Jim <jdeselms@vistaprint.com>
2022-02-25 23:02:30 -0500

    Starting to work on the calculateCost algorithm

--- src/calculateCost.js
+++ src/calculateCost.js
@@ -1,4 +1,5 @@
 import { COST, PROXY_CONTEXT } from "./symbols"
+import { getHandlerAndArgument } from "./getHandlerAndArgument"
 
 const DEFAULT_FUNCTION_COST = 10
 const DEFAULT_HANDLER_COST = 10
@@ -6,25 +7,31 @@ const DEFAULT_NODE_COST = 1
 
 export async function calculateCost(obj, ctx) {
 
-    if (obj[PROXY_CONTEXT]) {
-        obj = await resolve
-    }
+    // We want to make sure that we've got the raw values.
+    const rawValue = obj[PROXY_CONTEXT]
+        ? (await obj)[RAW_VALUE]
+        : obj
+
     const type = typeof rawValue
 
+    // Primitive types are essentially free
     if (rawValue === null || (type !== "object" && type !== "function")) {
         return 0
     }
 
+    // Try to get the cost off of the function; otherwise get the default.
     if (typeof rawValue === "function") {
         return rawValue[COST]?.() ?? DEFAULT_FUNCTION_COST
     }
 
-    const keys = Object.keys(rawValue)
-    let key, handler
-
-    if (keys.length === 1 && (key = keys[0]) && (handler = handlers?.[key])) {
-        return handler[COST]?.(rawValue[key]) ?? DEFAULT_HANDLER_COST
+    // If this is a handler, then we'll try to calculate the cost of the handler
+    // if it has a cost function. Otherwise we'll return the default.
+    const handlerAndArg = getHandlerAndArgument(rawValue, ctx.handlers)
+    if (handlerAndArg) {
+        return handlerAndArg.handler[COST]?.(handlerAndArg.argument) ?? DEFAULT_HANDLER_COST
     }
 
+    // This is just a boring node; we'll return a default value.
+    // (Q: Should we traverse it or is that a waste?)
     return DEFAULT_NODE_COST
 }
